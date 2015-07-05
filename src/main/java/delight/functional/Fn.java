7171e9ae5da45b8af7f2364a7d133ec2b5c49777
mxro <mxro@nowhere.com>
2015-07-05 11:31:44 +1000

    Moving projects to Java Delight Autocommit Sun Jul 05 11:31:44 EST 2015

--- src/main/java/delight/functional/Fn.java
+++ src/main/java/delight/functional/Fn.java
@@ -1,8 +1,5 @@
 package delight.functional;
 
-import delight.functional.Closure;
-import delight.functional.Function;
-import delight.functional.Success;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
--- src/main/java/delight/functional/collections/CollectionsUtils.java
+++ src/main/java/delight/functional/collections/CollectionsUtils.java
@@ -3,6 +3,7 @@ package delight.functional.collections;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
